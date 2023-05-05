29baf097d0d944d3d07885f01bd62cd119ea1f22
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2023-05-05 09:52:30 -0600

    Actualización, intento de implementación de widget con brillo

--- theme-change.js
+++ theme-change.js
@@ -1,4 +1,5 @@
 var icon = document.getElementById("theme-icon");
+var i = document.getElementById("theme-ib")
 var logo = document.getElementById("logo")
 var mancha = document.getElementById("back-img")
 
@@ -9,12 +10,16 @@ if (localStorage.getItem("theme")==null){
 let localData = localStorage.getItem("theme")
 
 if (localData == "light"){
-    icon.src = "images/moon.png"
+    i.classList.remove('fa-sun')
+    i.classList.add("fa-moon")
+    i.style.color="#1e1e1e"
     logo.src = "images/Logo-PDE-H14-light.png"
     mancha.src = "images/Mancha-light (1).png"
     document.body.classList.remove("dark-theme")
 } else if (localData == "dark"){
-    icon.src = "images/sun-light.png"
+    i.classList.remove("fa-moon")
+    i.classList.add("fa-sun")
+    i.style.color="#fff"
     logo.src = "images/Logo-PDE-H14-dark.png"
     mancha.src = "images/Mancha-dark (1).png"
     document.body.classList.add("dark-theme")
@@ -23,12 +28,16 @@ if (localData == "light"){
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")){
-        icon.src = "images/sun-light.png"
+        i.classList.remove("fa-moon")
+        i.classList.add("fa-sun")
+        i.style.color="#fff"
         logo.src = "images/Logo-PDE-H14-dark.png"
         mancha.src = "images/Mancha-dark (1).png"
         localStorage.setItem("theme", "dark")
     } else {
-        icon.src = "images/moon.png"
+        i.classList.remove('fa-sun')
+        i.classList.add("fa-moon")
+        i.style.color="#1e1e1e"
         logo.src = "images/Logo-PDE-H14-light.png"
         mancha.src = "images/Mancha-light (1).png"
         localStorage.setItem("theme", "light")
